2c5081cc12c4b3f20747bc4b3e87e62418c37ca4
gundul42 <gundul@mailbox.org>
2022-05-26 12:27:37 +0000

    almost fixed

--- srcs/requirements/wordpress/config/startup.sh
+++ srcs/requirements/wordpress/config/startup.sh
@@ -3,4 +3,6 @@ chown www-data:www-data /var/www/html
 unzip ./latest.zip && rm ./latest.zip
 chown  -R www-data:www-data /var/www/wordpress
 mv -n ./wordpress/* html && rmdir ./wordpress
+chown www-data:www-data /tmp/wp-config.php
+mv /tmp/wp-config.php ./html
 exec php-fpm7.3 -F
