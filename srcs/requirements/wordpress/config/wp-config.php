2c5a30064a1b55741207f9d0515114e7bd8c8475
gundul42 <gundul@mailbox.org>
2022-05-31 11:22:55 +0000

    redis cache done

--- srcs/requirements/wordpress/config/wp-config.php
+++ srcs/requirements/wordpress/config/wp-config.php
@@ -37,6 +37,11 @@ define( 'DB_CHARSET', 'utf8' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+/* Redis setup */
+define('WP_REDIS_PORT', 7777);
+define('WP_CACHE_KEY_SALT','MY_SITE_NICKNAME_HERE');
+define('WP_REDIS_HOST', 'redis');
+
 /**#@+
  * Authentication unique keys and salts.
  *
